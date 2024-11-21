f6d55229ed087a06114cd404e4036aa8f0d32fc4
MisterSPK <saip.konkimalla@gmail.com>
2024-11-20 19:04:17 -0500

    Completed the todo, and Net Blue Testing is completed

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MM_Net_Red_Testing.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MM_Net_Red_Testing.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -15,17 +16,25 @@ public class MM_Net_Red_Testing {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-36, -58, Math.toRadians(0)))
-                .lineToYSplineHeading(-36, Math.toRadians(270))
-                .waitSeconds(1)
-//                .setTangent(Math.toRadians(90))
-//                .lineToY(48)
-//                .setTangent(Math.toRadians(0))
-//                .lineToX(32)
-//                .strafeTo(new Vector2d(44.5, 30))
-//                .turn(Math.toRadians(180))
-//                .lineToX(47.5)
-                .waitSeconds(3)
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-36, -58, Math.toRadians(90)))
+                .setReversed(false)  // Unreversed trajectory has hooks on the start and end
+                .splineTo(new Vector2d(-50.0, -50.0), -Math.PI/4)
+                .waitSeconds(1.5)
+                .splineTo(new Vector2d(-23, -24.0), -Math.PI)
+                .turn(Math.PI )
+                .waitSeconds(1.5)
+                .splineTo(new Vector2d(-50.0, -50.0), -Math.PI/4)
+                .waitSeconds(1.5)
+                .splineTo(new Vector2d(-39, -24.0), -Math.PI)
+                .turn(Math.PI )
+                .waitSeconds(1.5)
+                .splineTo(new Vector2d(-50.0, -50.0), -Math.PI/4)
+                .waitSeconds(1.5)
+                .splineTo(new Vector2d(-52.0, -24.0), -Math.PI/4)
+                .turn(Math.PI/4)
+                .waitSeconds(1.5)
+                .splineTo(new Vector2d(-50.0, -50.0), -Math.PI/4)
+                .waitSeconds(1.5)
                 .build());
 
 
