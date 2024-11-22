3bc698c2e5dd32c5b9144e4596d331d6c700c9ea
dylan <dmsneade@gmail.com>
2024-11-22 17:51:59 -0500

    it makes a square using strafing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mm14691/MM14691AutoBox.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mm14691/MM14691AutoBox.java
@@ -1,24 +1,33 @@
 package org.firstinspires.ftc.teamcode.mm14691;
 
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.acmerobotics.roadrunner.ftc.Actions;
 
 @Autonomous
-public class MM14691AutoBox extends MM14691BaseOpMode {
+public class MM14691AutoBox extends MM14691BaseAuto {
+
+    // Create an instance of our params class so the FTC dash can manipulate it.
+    public static MM14691AutoBlueNet.Params PARAMS = new MM14691AutoBlueNet.Params();
+
 
     @Override
-    public void loop() {
+    public void start() {
+        super.start();
 
-        Actions.runBlocking(pinpointDrive.actionBuilder(initialPose)
-                .lineToX(47)
-                .turn(Math.toRadians(90))
-                .lineToY(46)
-                .turn(Math.toRadians(90))
-                .lineToX(-47)
-                .turn(Math.toRadians(90))
-                .lineToY(-46)
-                .turn(Math.toRadians(90))
+        runningActions.add(pinpointDrive.actionBuilder(getInitialPose())
+                .strafeTo(new Vector2d(-36,58))
+                .strafeTo(new Vector2d(-36,-58))
+                .strafeTo(new Vector2d(36,-58))
+                .strafeTo(new Vector2d(36,58))
                 .build());
 
+    }
+
+    @Override
+    public Pose2d getInitialPose() {
+        return new Pose2d(36,58, Math.toRadians(270));
+//        return new Pose2d(PARAMS.positionX, PARAMS.positionY, Math.toRadians(PARAMS.heading));
     }
 }
