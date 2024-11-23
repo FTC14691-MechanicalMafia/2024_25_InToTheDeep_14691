d5fa291dca9b81e558e8f92ae3eb6d8d4edff3ad
dylan <dmsneade@gmail.com>
2024-11-23 08:46:34 -0500

    stuff for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mm14691/MM14691AutoBoxRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mm14691/MM14691AutoBoxRed.java
@@ -5,10 +5,9 @@ import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.acmerobotics.roadrunner.ftc.Actions;
 
 @Autonomous
-public class MM14691AutoBox extends MM14691BaseAuto {
+public class MM14691AutoBoxRed extends MM14691BaseAuto {
 
     // Create an instance of our params class so the FTC dash can manipulate it.
     public static MM14691AutoBlueNet.Params PARAMS = new MM14691AutoBlueNet.Params();
@@ -21,26 +20,26 @@ public class MM14691AutoBox extends MM14691BaseAuto {
                 .strafeTo(new Vector2d(-36,58));
         TrajectoryActionBuilder second = first.endTrajectory()
                 .strafeTo(new Vector2d(-36,-58));
-        TrajectoryActionBuilder third = second.endTrajectory()
-                .strafeTo(new Vector2d(36,-58));
-        TrajectoryActionBuilder fourth = third.endTrajectory()
-                .strafeTo(new Vector2d(36,58));
+//        TrajectoryActionBuilder third = second.endTrajectory()
+//                .strafeTo(new Vector2d(36,-58));
+//        TrajectoryActionBuilder fourth = third.endTrajectory()
+//                .strafeTo(new Vector2d(36,58));
 
 
         runningActions.add(
                 new SequentialAction(
                         autoActionName("first"),
-                        first.build(),
-                armDrive.liftToDown(),
-                autoActionName("second"),
-                second.build(),
-                armDrive.liftToDown(),
-                autoActionName("third"),
-                third.build(),
-                armDrive.liftToDown(),
-                autoActionName("fourth"),
-                fourth.build()
-            )
+                        first.build()
+//                armDrive.liftToDown(),
+//                autoActionName("second"),
+//                second.build(),
+//                armDrive.liftToDown(),
+//                autoActionName("third"),
+//                third.build(),
+//                armDrive.liftToDown(),
+//                autoActionName("fourth"),
+//                fourth.build()
+                )
         );
 
     }
@@ -51,3 +50,4 @@ public class MM14691AutoBox extends MM14691BaseAuto {
 //        return new Pose2d(PARAMS.positionX, PARAMS.positionY, Math.toRadians(PARAMS.heading));
     }
 }
+
